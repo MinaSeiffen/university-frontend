483045c3d80ca48ba3ddf354648918ea1d0e3c1e
Yousab <usabemad40@gmail.com>
2024-05-20 22:58:06 +0300

    navbar home

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -8,14 +8,14 @@ export default function Navbar() {
           <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
             <img
               src="src/assets/Logo.png"
-              class="h-[103.55px] w-[200px]"
+              class="h-[103.55px] w-[200px] ms-20"
               alt="Flowbite Logo"
             />
           </a>
           <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button
               type="button"
-              class="text-white bg-[#240F6E] font-medium rounded-lg text-sm px-4 py-2 text-center"
+              class="text-white bg-[#240F6E] font-medium rounded-lg text-sm px-4 py-2 text-center me-20"
             >
               Sign Up
             </button>
