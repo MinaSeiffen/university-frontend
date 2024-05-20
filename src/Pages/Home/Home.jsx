98372f77fa1dbe91298167ce7db61d22a99f82cf
Caroline <carolinekaram2020@gmail.com>
2024-05-20 05:06:23 +0300

    Part of Home Page

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,145 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <div className="relative h-screen bg-[url('https://s3-alpha-sig.figma.com/img/2288/ef5f/612dd97ee2b87f34cb6d28c3569b07e9?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=RnU-X3Lwr2JnCbG2MJJ0~5B8gTAGnl9asBFkp2iGVuzo9LWAqQQFMCtbC06W62JbglJI3lLCJ7BFJmwPrNL0R9Y~tIgrnl~0bm9Xpxx-gr2AAt2tqIRukugCvJljNfPREcWOph5mczm3rJz0cBy8ygCxkQgo7pm5Ce~ktDbvNDMr6BOP4GYXT636swGGq~WJBBNPrq3qC5rVlFDF46JqQrGTiV41mgULL1x888i530igPH4m9dNk0RTtAAL7Buox3F~1a9742UmyIeFMdOCEdIhHNnO6f1yRTBBV6wdTZW1cUnm29uY2Hxyf7nndPSEtwyltTAeXSMo~8JCeX91rbw__')] bg-cover bg-center">
+        <div className="absolute  transform -translate-x-2/3 -translate-y-1/3 top-1/2 left-1/3">
+          <div className=" font-serif mb-0 text-white px-4">
+            <h1 className="text-7xl">
+              Discover your perfect <br />
+              route to studying at a <br />
+              top university abroad.
+            </h1>
+            <button className="text-white text-3xl font-normal bg-[#240F6E] rounded-lg m-5 p-3 pl-10 pr-10 ml-0 mt-9">
+              All Universities
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="">
+        <h2 className="text-[#5B4B92] font-serif text-5xl ml-28 mt-24 ">
+          Study in Russia: A Premier Educational Destination
+        </h2>
+        <div className="mt-14">
+          <h3 className="text-5xl font-serif ml-28 mt-8">
+            Discover Educational Opportunities in Russia
+          </h3>
+          <p className="text-3xl ml-28 mt-5 font-sans">
+            Russia is one of the most attractive study destinations for
+            international students, thanks to its <br /> world-ranked
+            universities and diverse academic programs. Russian universities
+            offer high-quality <br />
+            education in various disciplines, making them an excellent choice
+            for students seeking globally
+            <br /> recognized degrees.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col lg:flex-row items-center justify-center py-10 ">
+        <div className="lg:flex-1 flex flex-col items-start lg:items-center lg:text-left text-center lg py-8">
+          <h2 className="text-[#5B4B92] font-serif text-5xl -ml-24 mt-24  lg:text-5xl  lg:mt-0">
+            Benefits of Studying in Russia:
+          </h2>
+          <h1 className="text-6xl font-serif -ml-44 mt-8  lg:text-5xl">
+            1-Prestigious Universities
+          </h1>
+          <p className="  mt-5 font-sans lg:text-3xl leading-relaxed">
+            Russia boasts several prestigious universities that <br/> enjoy
+            international recognition and offer high- <br/> quality education along
+            with excellent research <br/> opportunities. Here are some of the
+            prestigious <br/> universities in Russia: Russia is home to some of <br/> the
+            oldest and most prestigious universities in <br/> the world, such as
+            Moscow State University and <br/> Saint Petersburg State University.
+          </p>
+        </div>
+        <div className="lg:flex-1 flex items-center justify-center lg:mt-28">
+          <img
+            className="w-1/2 lg:w-2/3 h-auto rounded-lg shadow-lg"
+            src="https://s3-alpha-sig.figma.com/img/b1f8/955e/4292a215cff6b8192eaa5fd8505c74ec?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hGCBhX1of6vnUVRARGFJK4Jy9w-TVmhFvIwCjMCQIUFEIPnAx~kSmCuXzwU~FkKRGNdnX9UJbq7YFSekAp-GSsjmTW2hgsqa~cQ443nXu2kyqdYyschWdWJ197IeimPlZwJJWSsdJbyJgtrEyf1b5haun~HRnyeteO4w75RqWgvzeC-OP60~kcH7BdeQmh8LreWwn0pVqMVcWejxP2FmU3AK7MRfuKm5xo8TzYvs4A86pdUMzy4Z3WRrCCV2r5zb8EtaGQZR1A86~V9tiY6blVLIzContEyWTM67lu4OaTPlTz-kRfn~lTQqsR8KuyA3rVLKKU70fn3oZcwrbEX3Lw__"
+            alt="Studying in Russia"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col lg:flex-row items-center justify-center py-10 ">
+      <div className="lg:flex-1 flex items-center justify-center lg:mt-28">
+          <img
+            className="w-1/2 lg:w-2/3 h-auto rounded-lg shadow-lg"
+            src="https://s3-alpha-sig.figma.com/img/e5ab/2def/b9de65316c97988d4e46e16152c52316?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=io1x14wdO~c40DXbeTUW7JlNKHEyOnH1fZjILD51Ol4I-cc8h71iPtwPuFjpwiRskdp6VMfihgv6j7JAf9JJGcqkZ-oM321s0ALPLNvQrloQehV9k6uMtnHXkbSB7XmxStWvSdAbo6boR~sWU4rJRo3pqQw-Kkaep91ahAwBigSyAt5ixqpruVlXuJrxWRrWc6jsnfuLdgYxiYI7XASvhJd5cQLE8ssfmsSHiQBxpVfzfiYTARfalsVCbtnuBsX2~cborA95j6hFodKno2Odl7M~1jDlwt26Eh5SP2rWP~lIMKiO8O4eohIAJM~8e4V0rtoRk784G1GEuKrfZKaOsA__"
+            alt="Studying in Russia"
+          />
+        </div>
+        <div className="lg:flex-1 flex flex-col items-start lg:items-center lg:text-left text-center lg py-8">
+          <h1 className="text-6xl font-serif -ml-44 mt-8  lg:text-5xl">
+          2-Diverse Study Programs
+          </h1>
+          <p className="  mt-5 font-sans lg:text-3xl leading-relaxed">
+          Russia offers an extensive array of study <br/> programs that cater to a wide range of academic <br/> interests and professional aspirations. With over <br/> 700 universities across the country, students can <br/> find programs in virtually every field of study. <br/> Whether you're interested in engineering, <br/> medicine, computer science, humanities, or the <br/>arts, Russian universities provide high-quality <br/>education and cutting-edge research <br/>opportunities.
+
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col lg:flex-row items-center justify-center py-10 ">
+        <div className="lg:flex-1 flex flex-col items-start lg:items-center lg:text-left text-center lg py-8">
+          <h1 className="text-6xl font-serif -ml-2 mt-8  lg:text-5xl">
+          3-Rich Cultural Experience
+          </h1>
+          <p className=" ml-14 mt-5 font-sans lg:text-3xl leading-relaxed">
+          Living and studying in Russia provides students <br/>with a unique opportunity to explore a rich and <br/> diverse culture and to learn about a long <br/>historical heritage.Russia is a country rich in <br/>culture, history, and tradition, offering visitors a<br/> truly immersive experience. From the grandeur<br/> of its historic landmarks to the vibrant arts<br/> scene, there's something for everyone to explore <br/>and enjoy. 
+          </p>
+        </div>
+        <div className="lg:flex-1 flex items-center justify-center lg:mt-20">
+          <img
+            className="w-1/2 lg:w-2/3 h-auto rounded-lg shadow-lg"
+            src="https://s3-alpha-sig.figma.com/img/4f6b/7a38/d57726a15940db16034161fa530cac90?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fgMe0WQYoqBRA-BD80gLip7FS~jUBY8WiE6QjGu9kKzPR1fbT3iW5UhfrTm8eB5yRcXcN35DiuYgg4y6H-uHpux~JElz0g7Cd9YhYqRi5H~abYINIJnGx5TJO9qYuEufFJbZVX2iPljF1t9jGe1XKkb9ehkDyYJWbw0MykRL~9W6tQqcXuTZ~4oWWaELLuiP3VRexzaqQIcI95AB047YFKo-XH4--ICWNEgXEw2wHpKiG2BofQAA1U~Q-8KpvQqUiiKK1pWfXruq-pQHdT2fxmPgk6i9Lm814yiDxr3sHKKx-dxIMgpqr9O-ap~VeIwG8tUFD92zrkTV48Jm~qhLTg__"
+            alt="Studying in Russia"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col lg:flex-row items-center justify-center py-10 ">
+      <div className="lg:flex-1 flex items-center justify-center lg:mt-16">
+          <img
+            className="w-1/2 lg:w-2/3 h-auto rounded-lg shadow-lg"
+            src="https://s3-alpha-sig.figma.com/img/3ba8/3642/f78937a025778c68dda46c6673bbc80a?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ZYQsfonVYlTL-shW9tNAYHBapLspXihrSr9Ve0IxrqMcrgSw8uy3mL3kBRmX3iDOkI7dcBO8ELnNJcKLuMwwzvV0RhUrfXGafC~LpyXMDOB1F4KE9JOow2flnPE9zCYxgjasoDuyLtVOBtm7Qsw2RmCkIqw81tZf4siLcDq2s5k3EaSywyhfBigqu2trqMqEXRJPW1MiXGkJx1LiAkemEVYDwE1tCeGSqK9S0RWu5o3uBZrCHTDtE8KV3aXtIbMKMWY~w-OkEP7hcQlJ6hCsxP8bZdzIFrSLny8fPs29Rr9rlrxinXpLK2mWk1E5lqhOnO~ghmiURVtvUJLUwlQz-w__"
+            alt="Studying in Russia"
+          />
+        </div>
+        <div className="lg:flex-1 flex flex-col items-start lg:items-center lg:text-left text-center lg py-8">
+          <h1 className="text-6xl font-serif -ml-36 mt-8  lg:text-5xl">
+          4-Learn the Russian Language
+          </h1>
+          <p className=" -ml-12 mt-5 font-sans lg:text-3xl leading-relaxed">
+          Studying in Russia provides students with a <br/> unique opportunity to immerse themselves in the <br/>Russian language, which is one of the most<br/> important languages in the world. Whether<br/> attending lectures, interacting with locals, or<br/> exploring the vibrant culture, students have<br/> countless opportunities to practice and improve<br/> their Russian language skills. Also it’s , one of the<br/> world's most important languages.
+          </p>
+        </div>
+      </div>
+
+      
+      {/* <div className="">
+        <h2 className="text-[#5B4B92] font-serif text-5xl ml-28 mt-24 ">
+        Some Student Success Stories
+        </h2>
+        <div className="flex flex-col lg:flex-row items-center justify-center py-10 ">
+      <div className="lg:flex-1 flex items-center justify-center lg:mt-16">
+          <img
+            className="w-1/2 lg:w-80 h-60 rounded-full shadow-lg"
+            src="https://s3-alpha-sig.figma.com/img/3ba8/3642/f78937a025778c68dda46c6673bbc80a?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ZYQsfonVYlTL-shW9tNAYHBapLspXihrSr9Ve0IxrqMcrgSw8uy3mL3kBRmX3iDOkI7dcBO8ELnNJcKLuMwwzvV0RhUrfXGafC~LpyXMDOB1F4KE9JOow2flnPE9zCYxgjasoDuyLtVOBtm7Qsw2RmCkIqw81tZf4siLcDq2s5k3EaSywyhfBigqu2trqMqEXRJPW1MiXGkJx1LiAkemEVYDwE1tCeGSqK9S0RWu5o3uBZrCHTDtE8KV3aXtIbMKMWY~w-OkEP7hcQlJ6hCsxP8bZdzIFrSLny8fPs29Rr9rlrxinXpLK2mWk1E5lqhOnO~ghmiURVtvUJLUwlQz-w__"
+            alt="Studying in Russia"
+          />
+        </div>
+        <div className="lg:flex-1 flex flex-col items-start lg:items-center lg:text-left text-center lg py-8">
+          <h1 className="text-6xl font-serif -ml-36 mt-8  lg:text-5xl">
+          4-Learn the Russian Language
+          </h1>
+          <p className=" -ml-12 mt-5 font-sans lg:text-3xl leading-relaxed">
+          Studying in Russia provides students with a <br/> unique opportunity to immerse themselves in the <br/>Russian language, which is one of the most<br/> important languages in the world. Whether<br/> attending lectures, interacting with locals, or<br/> exploring the vibrant culture, students have<br/> countless opportunities to practice and improve<br/> their Russian language skills. Also it’s , one of the<br/> world's most important languages.
+          </p>
+        </div>
+      </div>
+      </div> */}
+    </>
+  );
+}
