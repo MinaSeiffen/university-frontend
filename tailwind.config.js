98bcf477da622889f064cb945b64aae858d0f849
MinaE.Seiffen <minaseiffen@gmail.com>
2024-05-21 03:11:12 +0300

    updates with signup page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// Import the Tailwind CSS plugin
+const plugin = require('tailwindcss/plugin');
+
+// Export the Tailwind CSS configuration object
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -14,6 +17,5 @@ export default {
   },
   plugins: [
     require('flowbite/plugin')
-
   ],
-}
+};
