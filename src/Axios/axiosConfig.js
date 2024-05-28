10b8a91fdd3fb5e45f309b5b514fc18cdf00e2ef
MinaE.Seiffen <minaseiffen@gmail.com>
2024-05-28 22:56:24 +0300

    axios instance update

--- src/Axios/axiosConfig.js
+++ src/Axios/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://university-backend-qw4f.onrender.com',
   headers: {
     'Content-Type': 'multipart/form-data',
   },
