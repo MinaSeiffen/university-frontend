502935e1421e86febcb2ee6ed164386c9a82d9ec
osama4713 <osamatamer471@gmail.com>
2024-05-30 18:53:28 +0300

    new Updates

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -8,14 +8,14 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="bg-[#240F6E] text-[#FFFFFF] font-[Inter] mt-20">
-      <div className="mx-auto w-full max-w-screen-xl py-8 pt-20 pe-5">
+    <footer className="bg-[#240F6E] text-[#FFFFFF] font-[Inter] mt-20 ">
+      <div className="mx-auto py-8 pt-20 pe-5">
         <div className="flex justify-between">
           <div className="mb-6 md:mb-0 w-90">
-            <img src="/images/Frame 1.svg" className="h-[120px] me-3 -mt-[40px]" alt="FlowBite Logo" />
+            <img src="/images/Frame 1.svg" className="h-[120px] me-3 -mt-[40px] ml-[88px]" alt="FlowBite Logo" />
 
-            <p className="text-[16px] mt-10 mb-5">We help students find the right university in Russia.</p>
-            <div className="flex text-[20px]">
+            <p className="text-[16px] mt-10 mb-5 ml-[64px]">We help students find the right university in Russia.</p>
+            <div className="flex text-[20px] ml-[90px]">
               <a href="https://www.facebook.com" aria-label="Facebook" className='md:ml-[120px] mr-[24px]'>
                 < FaFacebook className="w-6" />
               </a>
@@ -29,7 +29,7 @@ function Footer() {
           </div>
 
           <div>
-            <h2 className="mb-6 text-[20px] font-semibold dark:text-white">Company</h2>
+            <h2 className="mb-6 text-[20px] font-semibold dark:text-white ">Company</h2>
             <ul className="dark:text-gray-400 font-medium">
               <li className="mb-4">
                 <p className="text-[16px]"><Link  to="#"> About STUDY ABROAD </Link></p>
@@ -56,7 +56,7 @@ function Footer() {
           </div>
 
           <div>
-            <h2 className="mb-6 text-[20px] font-semibold dark:text-white">Legal</h2>
+            <h2 className="mb-6 text-[20px] font-semibold dark:text-white mr-[160px]">Legal</h2>
             <ul className="dark:text-gray-400 font-medium">
               <li className="mb-4">
                 <p className="text-[16px]"><Link to="#"> Privacy Statement </Link></p>
@@ -72,14 +72,14 @@ function Footer() {
         </div>
         <div className='flex mt-20'>
 
-          <img className="ml-60 -mt-[24px]" src="/images/bicycle (1) 1.svg" alt="la_university" />
+          <img className="ml-[273px] -mt-[24px]" src="/images/bicycle (1) 1.svg" alt="la_university" />
 
-          <img className="ml-[650px]" src="/images/la_university.svg" alt="la_university" />
+          <img className="ml-[794px]" src="/images/la_university.svg" alt="la_university" />
 
-          <img className="ml-[50px]" src="/images/icon-park-outline_tree-two.svg " alt="la_university" />
+          <img className="ml-[43px]" src="/images/icon-park-outline_tree-two.svg " alt="la_university" />
 
         </div>
-        <hr className="-ml-[80px] -mt-[12px] w-[1440px]" />
+        <hr className="ml-[10px] -mt-[12px] w-full" />
       </div>
     </footer>
   );
