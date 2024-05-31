c8569213bdf754fb5d93572b00e521fd812ca040
MinaE.Seiffen <minaseiffen@gmail.com>
2024-05-31 23:41:26 +0300

    emails contact us

--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ContactCard = ({title , phoneNum}) => {
+const ContactCard = ({title , phoneNum ,email}) => {
   return (<>
     <div className='lg:w-[511px] lg:h-[227px] rounded-[10px] p-[44px] flex lg:flex-col lg:gap-[44px] bg-[#C8E1FC] '>
         <p className='lg:w-[331px] lg:h-[30px] font-merriweather font-normal text-[24px] leading-[30.17px] text-[#212121] '>
@@ -16,7 +16,7 @@ const ContactCard = ({title , phoneNum}) => {
             <div className='lg:w-[249px] lg:h-[65px] flex flex-col lg:gap-[8px] '>
             <img src="/images/email.svg" alt="whatsapp" width={32} height={32} className='mx-auto' />
             <p className='lg:w-[249px] lg:h-[25px] font-merriweather font-normal text-[20px] leading-[25.14px] text-[#212121] '>
-            studyabroad@gmail.com
+            {email}
             </p>
             </div>
         </div>
